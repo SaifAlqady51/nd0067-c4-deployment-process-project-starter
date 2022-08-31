4e483302490e93fd8cacf71da1bd2027866e3699
SaifAlqady <siafalqady51@gmail.com>
2022-08-31 09:24:28 +0200

    sequelize

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port :config.postgres_port,
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(
+  "postgres://postgres:password@database-udagram.cgtnrc6jnrh7.us-east-1.rds.amazonaws.com:5432/postgres"
+);
+
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port :config.postgres_port,
+
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
