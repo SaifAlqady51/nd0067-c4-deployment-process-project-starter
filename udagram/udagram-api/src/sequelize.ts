6e276cbb46dc78517fe083eed446bfced4f729aa
SaifAlqady <siafalqady51@gmail.com>
2022-08-31 18:10:09 +0200

    connect rds

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -11,12 +11,13 @@ import { config } from "./config/config";
 //   `postgres://postgres:password@database-udagram.cgtnrc6jnrh7.us-east-1.rds.amazonaws.com:5432/postgres`
 // )
 
+
+
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-  port :config.postgres_port,
 
   dialect: "postgres",
   storage: ":memory:",
