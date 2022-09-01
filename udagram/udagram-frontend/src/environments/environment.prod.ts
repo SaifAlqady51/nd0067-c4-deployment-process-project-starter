07c110ef0eecf2eaa5ead69224e903dc5f5ba549
SaifAlqady <siafalqady51@gmail.com>
2022-09-01 17:30:57 +0200

    url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-dev.eba-svv2mprx.us-east-1.elasticbeanstalk.com//api/v0',
+  apiHost: 'http://udagram-dev.eba-svv2mprx.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
