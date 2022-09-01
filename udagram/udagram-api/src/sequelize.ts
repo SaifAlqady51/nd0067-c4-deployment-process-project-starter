7569f549db518635cdab6365bf6a68aacade5daa
SaifAlqady <siafalqady51@gmail.com>
2022-09-01 08:00:04 +0200

    port edit

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -3,7 +3,7 @@ import { config } from "./config/config";
 
 
 export const sequelize = new Sequelize(
-  `postgres://${config.username}:${config.rds_password}@${config.rds}:${config.port}/${config.username}`
+  `postgres://${config.username}:${config.rds_password}@${config.rds}:${config.postgres_port}/${config.username}`
 )
 
 
